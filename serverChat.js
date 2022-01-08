803930fdb1b38d0ae45a498fc4135a90ea3ab403
Bogdan243 <44081819+Bogdan243@users.noreply.github.com>
2022-01-08 13:52:37 +0300

    Added log in page

--- serverChat.js
+++ serverChat.js
@@ -1,17 +1,37 @@
-const bodyParser = require('body-parser')
 const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 var app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/autorization'))
+app.use(express.static(__dirname + '/public/chat'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/main.html')
+  res.sendFile(__dirname + '/public/autorization/index.html')
+})
+
+let users = [];
+
+app.post('/register', urlencodedParser, function (
+  request,
+  response
+) {
+  if (!request.body) return response.sendStatus(400)
+  console.log(request.body)
+
+  users.push({
+    userName: request.body.userName,
+    pasword: request.body.userPassword
+  })
+  
+  res.sendFile(__dirname + '/public/chat/main.html')
 })
 
 let messageList = []
